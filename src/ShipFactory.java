b8391c92e4baa0c979e2beb15fc1da71b41b0a34
Matthew Sargeant <matthewsargeant@matthews-mbp.cvtc.local>
2018-03-07 13:30:10 -0600

    got the testCanCreateCarrierShips to run

--- src/ShipFactory.java
+++ src/ShipFactory.java
@@ -22,6 +22,11 @@ public class ShipFactory {
 		case BATTLESHIP:
 			ship = new Ship(4);
 			break;
+			
+		case CARRIER:
+			ship = new Ship(5);
+			break;
+			
 		default:
 			throw new Exception("Undefined ShipType: " + type);
 		}

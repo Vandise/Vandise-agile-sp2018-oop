0ce79627534a36be5b060bbc88b6a5a08f91ac26
Casey Capozzi <caseycapozzi@ymail.com>
2018-03-07 14:32:04 -0600

    Add call to hit() inside update() in order to pass test case testShipUpdateCallsHit()

--- src/Ship.java
+++ src/Ship.java
@@ -7,12 +7,9 @@ public class Ship implements Observable {
 		this.sunk = false;
 	}
 
-	// TODO
-	// See unit tests for what needs to be done here
-	// in the observer pattern
-	//
-	public void update() throws Exception {
 
+	public void update() throws Exception {
+		this.hit();
 	}
 
 	public boolean isSunk() {
@@ -23,9 +20,6 @@ public class Ship implements Observable {
 		return this.life;
 	}
 
-	// TODO
-	//	a peice of logic is missing here
-	//
 	public void hit() throws Exception {
 		
 		if (!this.sunk) {

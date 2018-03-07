03f4c38e803f523cd6f6eaeb30a2bd8c20a8e659
Drewry Pope <drew-pope@idexx.com>
2018-03-07 13:21:34 -0600

    feature-coordinate fix canBeHit test (and missesWhenNoShip in the process)

--- src/Coordinate.java
+++ src/Coordinate.java
@@ -8,39 +8,28 @@ public class Coordinate extends CoordinateSubject {
 		this.hasShip = false;
 	}
 
-	// TODO
-	// call the coordinate, check if the shit was hit, missed, or sunk
-	// read about the observer pattern
-	//
 	public String call() throws Exception {
-		if (!this.called) {
-			this.notifyObserver();
-			return "Miss";
-		} else {
+		if (this.called) {
 			throw new Exception("Coordinate already called");
+		} else if (this.hasShip) {
+			this.notifyObserver();
+			return "Hit";
+		}  else {
+			return "Miss";
 		}
 	}
 
-	// TODO
-	//	read about inheritance and calling parent
-	//	methods in Java
-	//
 	@Override
 	public void attach(Ship observer) {
-		// TODO
+		super.attach(observer);
+		this.hasShip = true;
 	}
 
-	// TODO
-	// return if the coordinate has been called or not
-	//
 	public boolean called() {
 		return false;
 	}
 
-	// TODO
-	// is there a ship on this coordinate?
-	//
 	public boolean hasShip() {
-		return false;
+		return this.hasShip;
 	}
 }
--- src/CoordinateSubject.java
+++ src/CoordinateSubject.java
@@ -4,12 +4,8 @@
 public abstract class CoordinateSubject {
 	protected Ship observer;
 
-	// TODO
-	// update the observer
-	// read about the observer pattern
-	//
 	public void notifyObserver() throws Exception {
-
+		observer.update();
 	}
 
 	public Ship getObservable() {

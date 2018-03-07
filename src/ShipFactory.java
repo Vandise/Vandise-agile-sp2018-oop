66b35a634dff8111c0d7bd7e86a12e8fa368ad1a
Cody Bennett <codybennett@codys-mbp.cvtc.local>
2018-03-07 13:35:58 -0600

    Fixed the patrol shit test so it was passing.

--- src/ShipFactory.java
+++ src/ShipFactory.java
@@ -6,10 +6,15 @@ public class ShipFactory {
 	//
 	public static Ship create(ShipType type) throws Exception {
 		Ship ship = null;
-		switch(type) {
+		switch(type) { 
+		case PATROL:
+		    ship = new Ship(2);
+		    break;
+		    
 		case BATTLESHIP:
 			ship = new Ship(4);
 			break;
+
 		default:
 			throw new Exception("Undefined ShipType: " + type);
 		}

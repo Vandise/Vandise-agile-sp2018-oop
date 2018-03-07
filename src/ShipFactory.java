7d888150eff890bdb7bf2d60e30a39773aa978af
Sam Peterson <speterson96@student.cvtc.edu>
2018-03-07 14:01:41 -0600

    Added ship types into ShipFactory.java allowing them to pass ShipFactoryTest.java

--- src/ShipFactory.java
+++ src/ShipFactory.java
@@ -6,9 +6,23 @@ public class ShipFactory {
 	//
 	public static Ship create(ShipType type) throws Exception {
 		Ship ship = null;
+		
 		switch(type) {
+		
 		case BATTLESHIP:
 			ship = new Ship(4);
+			break;
+		case CARRIER:
+			ship = new Ship(5);
+			break;
+		case DESTROYER:
+			ship = new Ship(3);
+			break;
+		case PATROL:
+			ship = new Ship(2);
+			break;
+		case SUBMARINE:
+			ship = new Ship(3);
 			break;
 		default:
 			throw new Exception("Undefined ShipType: " + type);

ae73a9f30ace50940938644fe17e96fb0934d43f
Matthew Sargeant <matthewsargeant@matthews-mbp.cvtc.local>
2018-03-07 13:24:30 -0600

    made the testCanCreatePatrolShips pass

--- src/ShipFactory.java
+++ src/ShipFactory.java
@@ -7,6 +7,10 @@ public class ShipFactory {
 	public static Ship create(ShipType type) throws Exception {
 		Ship ship = null;
 		switch(type) {
+		case PATROL:
+			ship = new Ship(2);
+			break;
+		
 		case BATTLESHIP:
 			ship = new Ship(4);
 			break;

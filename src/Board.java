a4cce9f4dc0ab8c6ab59789200089f6c711918d1
Jon Fehr <jfehr4@student.cvtc.edu>
2018-03-07 15:55:14 -0600

    Added commits and tired to add coordinates

--- src/Board.java
+++ src/Board.java
@@ -28,6 +28,7 @@ public class Board {
 		if(ShipType.BATTLESHIP == type) {
 			this.size = 4;
 			this.shipCount ++;
+			coordinateVector.add(x,y);
 		}
 		else if(ShipType.CARRIER == type) {
 			this.size = 5;
@@ -46,8 +47,10 @@ public class Board {
 			this.shipCount ++;
 		}
 		
-		
-	
+		//I have tried to added the coordinates into the vector so they can then be check to see if there is a shipt there
+		//if there was a ship there I would then have it show that the ship was hit. With the incorporated size above
+		// I would be able to then check to see if all the spots on the ship was hit or not. If it was i would then call
+		// call the sunk method to show that it was sunk. 
 		
 	}
 

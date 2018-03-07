2a5ac628af1344f38a64df0f34cd1e539f9759ea
William Ramirez <wramirezramos@student.cvtc.edu>
2018-03-07 15:19:45 -0600

    Fixed isSunk

--- src/Ship.java
+++ src/Ship.java
@@ -12,10 +12,20 @@ public class Ship implements Observable {
 	// in the observer pattern
 	//
 	public void update() throws Exception {
+		if (!this.sunk) {
+			this.life -= 1;
+		} else {
+			throw new Exception("Hit on ship that's already sunk.");
+		}
 
 	}
 
 	public boolean isSunk() {
+		if (this.life < 1 ){
+			this.sunk = true;
+		}else {
+			this.sunk = false;
+		}
 		return this.sunk;
 	}
 
@@ -27,9 +37,10 @@ public class Ship implements Observable {
 	//	a peice of logic is missing here
 	//
 	public void hit() throws Exception {
-		if (!this.sunk) {
+		if ((!this.sunk)) {
 			this.life -= 1;
 		} else {
+			this.sunk = true;
 			throw new Exception("Hit on ship that's already sunk.");
 		}
 	}

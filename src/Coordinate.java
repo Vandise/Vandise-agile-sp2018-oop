c5ac10b1d314d02b5d8ec5894929fc77ea50e982
jessicavollmer <jessica@capturelifellc.com>
2018-03-07 13:41:04 -0600

    Calling coordinate sets test passed

--- src/Coordinate.java
+++ src/Coordinate.java
@@ -15,6 +15,7 @@ public class Coordinate extends CoordinateSubject {
 	public String call() throws Exception {
 		if (!this.called) {
 			this.notifyObserver();
+			this.called = true;
 			return "Miss";
 		} else {
 			throw new Exception("Coordinate already called");
@@ -34,6 +35,9 @@ public class Coordinate extends CoordinateSubject {
 	// return if the coordinate has been called or not
 	//
 	public boolean called() {
+		if(called) {
+			return true;
+		}
 		return false;
 	}
 

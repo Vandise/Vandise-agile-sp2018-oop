212316cee69082b57089bebfa66d489776db4ed0
Sam Peterson <speterson96@student.cvtc.edu>
2018-03-07 14:13:04 -0600

    Added logic for test on CannotHitShipMoreThanMaxLife

--- src/Ship.java
+++ src/Ship.java
@@ -27,7 +27,7 @@ public class Ship implements Observable {
 	//	a peice of logic is missing here
 	//
 	public void hit() throws Exception {
-		if (!this.sunk) {
+		if (this.life > 0) {
 			this.life -= 1;
 		} else {
 			throw new Exception("Hit on ship that's already sunk.");

ec18ffb6f001e22e7de8449fcaa0ee006c16fd65
MitchellKohrs <35751723+MitchellKohrs@users.noreply.github.com>
2018-03-07 13:25:34 -0600

    passed the test to create a patrol boat with the length of 2

--- src/ShipFactory.java
+++ src/ShipFactory.java
@@ -7,9 +7,15 @@ public class ShipFactory {
 	public static Ship create(ShipType type) throws Exception {
 		Ship ship = null;
 		switch(type) {
+		
 		case BATTLESHIP:
 			ship = new Ship(4);
 			break;
+		
+		case PATROL:
+      ship = new Ship(2);
+      break;
+			
 		default:
 			throw new Exception("Undefined ShipType: " + type);
 		}
